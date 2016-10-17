83f253e0b0187212179e92d4407982618f2318b2
Connor <noxor0@users.noreply.github.com>
2016-10-16 19:25:49 -0700

    Update PulseSensor_Spark.cpp

--- firmware/PulseSensor_Spark.cpp
+++ firmware/PulseSensor_Spark.cpp
@@ -1,96 +1,84 @@
-#include "application.h"
-#include "PulseSensor_Spark.h"
+// This #include statement was automatically added by the Particle IDE.
+#include "DS18B20/DS18B20.h"
+#include "DS18B20/Particle-OneWire.h"
+#include "Particle.h"
 
-void interruptSetup(void){
-  // Allocate a timer to throw an interrupt every 2mS.
-  pulseTimer.begin(pulseISR, 2000, uSec);  // blinkLED to run every 2ms (2000 * 1us period)
+//Varible setup 
+DS18B20 ds18b20 = DS18B20(D6); //Sets Pin D2 for Water Temp Sensor
+int led = D7;
+char szInfo[64];
+float pubTemp;
+double celsius;
+double fahrenheit;
+unsigned int Metric_Publish_Rate = 6000;
+unsigned int MetricnextPublishTime;
+int DS18B20nextSampleTime;
+int DS18B20_SAMPLE_INTERVAL = 2500;
+int dsAttempts = 0;
 
-} 
+void interruptSetup(void);
+extern volatile int BPM;
 
+//Choose webhook
+const char *PUBLISH_EVENT_NAME = "FB-Sensors";
 
-// THIS IS THE TIMER (2 on Core, 3 on Photon) INTERRUPT SERVICE ROUTINE. 
-// The Timer makes sure that we take a reading every 2 miliseconds
-//ISR(TIMER2_COMPA_vect){                         // triggered when Timer2 counts to 124
-void pulseISR(void) {
-  noInterrupts();
-  Signal = analogRead(pulsePin);              // read the Pulse Sensor 
-  sampleCounter += 2;                         // keep track of the time in mS with this variable
-  int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
+void setup() {
+    Time.zone(-5);
+    Particle.syncTime();
+    pinMode(D2, INPUT);
+    Serial.begin(115200);
+    interruptSetup();
+    
+}
 
-    //  find the peak and trough of the pulse wave
-  if(Signal < thresh && N > (IBI/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
-    if (Signal < T){                        // T is the trough
-      T = Signal;                         // keep track of lowest point in pulse wave 
-    }
-  }
-
-  if(Signal > thresh && Signal > P){          // thresh condition helps avoid noise
-    P = Signal;                             // P is the peak
-  }                                        // keep track of highest point in pulse wave
-
-  //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
-  // signal surges up in value every time there is a pulse
-  if (N > 250){                                   // avoid high frequency noise
-    if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
-      Pulse = true;                               // set the Pulse flag when we think there is a pulse
-      digitalWrite(blinkPin,HIGH);                // turn on blink LED
-      IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
-      lastBeatTime = sampleCounter;               // keep track of time for next pulse
-
-      if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
-        secondBeat = false;                  // clear secondBeat flag
-        for(int i=0; i<=9; i++){             // seed the running total to get a realisitic BPM at startup
-          rate[i] = IBI;                      
-        }
-      }
-
-      if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
-        firstBeat = false;                   // clear firstBeat flag
-        secondBeat = true;                   // set the second beat flag
-		interrupts();
-        return;                              // IBI value is unreliable so discard it
-      }   
-
-
-      // keep a running total of the last 10 IBI values
-      uint16_t runningTotal = 0;              // clear the runningTotal variable    
-
-      for(int i=0; i<=8; i++){                // shift data in the rate array
-        rate[i] = rate[i+1];                  // and drop the oldest IBI value 
-        runningTotal += rate[i];              // add up the 9 oldest IBI values
-      }
-
-      rate[9] = IBI;                          // add the latest IBI to the rate array
-      runningTotal += rate[9];                // add the latest IBI to runningTotal
-      runningTotal /= 10;                     // average the last 10 IBI values 
-      BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
-      QS = true;                              // set Quantified Self flag 
-      // QS FLAG IS NOT CLEARED INSIDE THIS ISR
-    }                       
-  }
-
-  if (Signal < thresh && Pulse == true){   // when the values are going down, the beat is over
-    digitalWrite(blinkPin,LOW);            // turn off pin blink LED
-    Pulse = false;                         // reset the Pulse flag so we can do it again
-    amp = P - T;                           // get amplitude of the pulse wave
-    thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
-    P = thresh;                            // reset these for next time
-    T = thresh;
-  }
-
-  if (N > 2500){                           // if 2.5 seconds go by without a beat
-    thresh = 512;                          // set thresh default
-    P = 512;                               // set P default
-    T = 512;                               // set T default
-    lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
-    firstBeat = true;                      // set these to avoid noise
-    secondBeat = false;                    // when we get the heartbeat back
-  }
-
+void loop() {
+if (millis() > DS18B20nextSampleTime){
     interrupts();
-}// end isr
+    getTemp();
+    noInterrupts();
+  }
+  if (millis() > MetricnextPublishTime){
+    Serial.println("Publishing now.");
+    publishData();
+  }
+}
 
 
+void publishData(){
+  if(!ds18b20.crcCheck()){
+    return;
+  }
+  char tempArr[64];
+  sprintf(tempArr, "%d", BPM);
 
+  sprintf(szInfo, "%2.2f", fahrenheit);
+  strcat(szInfo, ",30");
+  //publish temp
+  Particle.publish(PUBLISH_EVENT_NAME, szInfo, PRIVATE);
+  //set time
+  MetricnextPublishTime = millis() + Metric_Publish_Rate;
+}
 
-
+void getTemp(){
+    if(!ds18b20.search()){
+      ds18b20.resetsearch();
+      celsius = ds18b20.getTemperature();
+      Serial.println(celsius);
+      while (!ds18b20.crcCheck() && dsAttempts < 4){
+        Serial.println("Caught bad value.");
+        dsAttempts++;
+        Serial.print("Attempts to Read: ");
+        Serial.println(dsAttempts);
+        if (dsAttempts == 3){
+          delay(1000);
+        }
+        ds18b20.resetsearch();
+        celsius = ds18b20.getTemperature();
+        continue;
+      }
+      dsAttempts = 0;
+      fahrenheit = ds18b20.convertToFahrenheit(celsius);
+      DS18B20nextSampleTime = millis() + DS18B20_SAMPLE_INTERVAL;
+      Serial.println(fahrenheit);
+    }
+}
